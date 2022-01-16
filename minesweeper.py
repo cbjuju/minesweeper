dce253c76fe60c99bdaa82250becc0a9111a8cd9
Sudip Kunda <sudipkunda@gmail.com>
2022-01-16 19:29:12 +0530

    Initial stages of GUI from pygame.
    Pygame module imported and initialized. Game window dimensions set and
    game window initialized.

--- minesweeper.py
+++ minesweeper.py
@@ -1,5 +1,10 @@
 import backend
+import pygame as pg
 
 newGame = backend.minesweeperGame()
 newGame.printGreeting()
 newGame.makeMap(2, 2, 1)
+
+pg.init()
+width, height = 900, 400
+screen = pg.display.set_mode((width, height))
