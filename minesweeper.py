0e60a4a0d166443c2499c7ca14e02077b640f942
Sudip Kunda <sudipkunda@gmail.com>
2022-01-18 16:49:46 +0530

    chore: add image and feat: first image blit

--- minesweeper.py
+++ minesweeper.py
@@ -9,6 +9,10 @@ pygame.init()
 width, height = 900, 400
 screen = pygame.display.set_mode((width, height))
 
+mineImage = pygame.image.load("images/Mine.png")
+screen.blit(mineImage, (100, 100))
+pygame.display.flip()
+
 running = True
 
 while running:
