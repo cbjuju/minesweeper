914ef40f9727ed9e08120a8e8e1848047ef78e76
Sudip Kunda <sudipkunda@gmail.com>
2022-01-19 18:32:21 +0530

    Feat : Create blinking animation.
    Once you blit an image on the screen, it cannot simply be erased. The
    whole screen must be filled with black and the objects redrawn. Will
    look into a more sophisticated version of this next using dirty
    rectangles.

--- minesweeper.py
+++ minesweeper.py
@@ -1,3 +1,4 @@
+import time
 import backend
 import pygame
 
@@ -9,13 +10,26 @@ pygame.init()
 width, height = 900, 400
 screen = pygame.display.set_mode((width, height))
 
-mineImage = pygame.image.load("images/Mine.png")
-screen.blit(mineImage, (100, 100))
-pygame.display.flip()
+mineImage = pygame.image.load("01_01_first_blit/01_image.png")
 
-running = True
+for i in range(10):
+    mineImage.set_alpha(256)
+    screen.blit(mineImage, (50 + 5 * i, 50))
+    pygame.display.flip()
+    time.sleep(0.5)
+    screen.fill((0, 0, 0))
 
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+# for i in range(10):
+#     mineImage.set_alpha(256)
+#     pygame.display.flip()
+#     time.sleep(0.5)
+#     mineImage.set_alpha(128)
+#     pygame.display.flip()
+#     time.sleep(0.5)
+
+# running = True
+
+# while running:
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             running = False
