32e809f1de826338c46045cfc790d4debaa0ecd1
Sudip Kunda <sudipkunda@gmail.com>
2022-01-28 16:33:06 -0600

    Feat: Add new minesweeper tiles. Load initial screen with new tiles.

--- minesweeper.py
+++ minesweeper.py
@@ -14,6 +14,96 @@ class minesweeper_game:
         self.pixel_width_of_box  = 50
         self.pixel_height_of_box = 50
 
+        # Initialize the graphical element simulating the minefield
+        pygame.init()
+        width       = number_of_cols * self.pixel_width_of_box
+        height      = number_of_rows * self.pixel_height_of_box
+        self.screen = pygame.display.set_mode((width, height))
+
+        self.exploded_box_image = pygame.image \
+            .load("images/minesweeper_tiles/exploded_box.jpg") \
+            .convert_alpha()
+
+        self.covered_box_image = pygame.image \
+            .load("images/minesweeper_tiles/covered_box.jpg") \
+            .convert_alpha()
+
+        self.empty_box_image = pygame.image \
+            .load("images/minesweeper_tiles/empty_box.jpg") \
+            .convert_alpha()
+
+        self.flagged_box_image = pygame.image \
+            .load("images/minesweeper_tiles/flagged_box.jpg") \
+            .convert_alpha()
+
+        self.one_image = pygame.image \
+            .load("images/minesweeper_tiles/one.jpg") \
+            .convert_alpha()
+
+        self.two_image = pygame.image \
+            .load("images/minesweeper_tiles/two.jpg") \
+            .convert_alpha()
+
+        self.three_image = pygame.image \
+            .load("images/minesweeper_tiles/three.jpg") \
+            .convert_alpha()
+
+        self.four_image = pygame.image \
+            .load("images/minesweeper_tiles/four.jpg") \
+            .convert_alpha()
+
+        self.five_image = pygame.image \
+            .load("images/minesweeper_tiles/five.jpg") \
+            .convert_alpha()
+
+        self.six_image = pygame.image \
+            .load("images/minesweeper_tiles/six.jpg") \
+            .convert_alpha()
+
+        self.seven_image = pygame.image \
+            .load("images/minesweeper_tiles/seven.jpg") \
+            .convert_alpha()
+
+        self.eight_image = pygame.image \
+            .load("images/minesweeper_tiles/eight.jpg") \
+            .convert_alpha()
+
+        self.exploded_box_image = pygame.transform.scale(self.exploded_box_image,
+                (self.pixel_width_of_box, self.pixel_height_of_box))
+
+        self.covered_box_image = pygame.transform.scale(self.covered_box_image,
+                (self.pixel_width_of_box, self.pixel_height_of_box))
+
+        self.empty_box_image = pygame.transform.scale(self.empty_box_image,
+                (self.pixel_width_of_box, self.pixel_height_of_box))
+
+        self.flagged_box_image = pygame.transform.scale(self.flagged_box_image,
+                (self.pixel_width_of_box, self.pixel_height_of_box))
+
+        self.one_image = pygame.transform.scale(self.one_image,
+                (self.pixel_width_of_box, self.pixel_height_of_box))
+
+        self.two_image = pygame.transform.scale(self.two_image,
+                (self.pixel_width_of_box, self.pixel_height_of_box))
+
+        self.three_image = pygame.transform.scale(self.three_image,
+                (self.pixel_width_of_box, self.pixel_height_of_box))
+
+        self.four_image = pygame.transform.scale(self.four_image,
+                (self.pixel_width_of_box, self.pixel_height_of_box))
+
+        self.five_image = pygame.transform.scale(self.five_image,
+                (self.pixel_width_of_box, self.pixel_height_of_box))
+
+        self.six_image = pygame.transform.scale(self.six_image,
+                (self.pixel_width_of_box, self.pixel_height_of_box))
+
+        self.seven_image = pygame.transform.scale(self.seven_image,
+                (self.pixel_width_of_box, self.pixel_height_of_box))
+
+        self.eight_image = pygame.transform.scale(self.eight_image,
+                (self.pixel_width_of_box, self.pixel_height_of_box))
+
     def initialize_the_backend(self):
 
         # Generate the data structure simulating the minefield
@@ -23,53 +113,46 @@ class minesweeper_game:
                 self.number_of_mines)
 
     def initialize_the_frontend(self):
-        # generate graphical element simulating the minefield
-        pygame.init()
-        width               = number_of_cols * self.pixel_width_of_box
-        height              = number_of_rows * self.pixel_height_of_box
-        screen              = pygame.display.set_mode((width, height))
 
         # fill background with a grey colour
-        screen.fill((128, 128, 128))
+        self.screen.fill((128, 128, 128))
         # draw overlay
         for i in range(number_of_cols - 1):
-            pygame.draw.line(screen,
+            pygame.draw.line(self.screen,
                     (0, 0, 0),
                     (self.pixel_width_of_box * (i + 1), 0),
                     (self.pixel_width_of_box * (i + 1), self.pixel_height_of_box *
                         number_of_rows))
         for i in range(number_of_cols - 1):
-            pygame.draw.line(screen,
+            pygame.draw.line(self.screen,
                     (0, 0, 0),
                     (0, self.pixel_height_of_box * (i + 1)),
                     (self.pixel_width_of_box * number_of_cols, 
                         self.pixel_height_of_box * (i + 1)))
 
-        flagImage = pygame.image.load("images/Flag.png").convert_alpha()
-        flagImage = pygame.transform.scale(flagImage,
-                (self.pixel_width_of_box, self.pixel_height_of_box))
-
         for box in self.new_game.the_map:
-            if box['has_mine']:
-                xposition = box['xposition'] * self.pixel_width_of_box
-                yposition = box['yposition'] * self.pixel_height_of_box
-                rectangle = pygame.Rect(xposition, 
-                        yposition,
-                        self.pixel_width_of_box,
-                        self.pixel_height_of_box)
+            xposition = box['xposition'] * self.pixel_width_of_box
+            yposition = box['yposition'] * self.pixel_height_of_box
+            rectangle = pygame.Rect(xposition, 
+                    yposition,
+                    self.pixel_width_of_box,
+                    self.pixel_height_of_box)
 
-                screen.blit(flagImage, rectangle)
+            self.screen.blit(self.covered_box_image, rectangle)
+
+        #         screen.blit(mine_image, rectangle)
+        #     if box['has_mine']:
+        #         xposition = box['xposition'] * self.pixel_width_of_box
+        #         yposition = box['yposition'] * self.pixel_height_of_box
+        #         rectangle = pygame.Rect(xposition, 
+        #                 yposition,
+        #                 self.pixel_width_of_box,
+        #                 self.pixel_height_of_box)
+
+        #         screen.blit(mine_image, rectangle)
         
         # update the display to show overlay
         pygame.display.update()
-
-        # for box in self.new_game.the_map:
-        box = self.new_game.the_map[0]
-        xposition = box['xposition'] * self.pixel_width_of_box
-        yposition = box['yposition'] * self.pixel_height_of_box
-        print(f"x = {xposition} y = {yposition}")
-        # pyautogui.moveTo(xposition, yposition)
-        time.sleep(1)
 
     def start_game(self):
         # Start game loop
@@ -93,10 +176,7 @@ class minesweeper_game:
                     col_number = x_position // self.pixel_width_of_box
                     row_number = y_position // self.pixel_height_of_box
 
-                    print(f"row number = {row_number} col number = {col_number}")
-                    box_number = row_number * number_of_cols + col_number
-                    has_mine = self.new_game.the_map[box_number]['has_mine']
-                    if has_mine: print(f"has mine")
+                    # box_number = row_number * number_of_cols + col_number
 
     def initialize(self):
         self.initialize_the_backend()
